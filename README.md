72e3533d09bc75f2c2184f3b4a3a56cf8e6431da
Kendall Purser <kendallpurser@gmail.com>
2015-06-05 15:52:12 -0600

    fife updates

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ The Free Interactive Fiction Engine (fife) is a JavaScript engine for creating a
 
 Purpose/Goal
 ---
-To make interactive fiction easier to write and more accessible on the web. This is done by creating a user interface that is mobile friendly and utilizes JavaScript as the language of the interpreter. Stories can then be written in JSON format using a predefined structure that the interpreter understands. The end result will be introducing interactive fiction to a new generation that uses mobile devices as their primary form of entertainment.
+To make interactive fiction more accessible on the web. This is done by creating a user interface that is mobile friendly and utilizes JavaScript as the language of the interpreter. Stories can then be written in JSON format using a predefined structure that the interpreter understands. The end result will be introducing interactive fiction to a new generation that uses mobile devices as their primary form of entertainment.
 
 How to use fife
 ---
--- fife.js
+++ fife.js
@@ -49,63 +49,109 @@ var fife = {
 		get : ["take"],
 		i : ["inventory"],
 		n : ["north"],
-		e : ["east"],
-
-
+		e : ["east"]
 	}
 	commands : {
 		// movement
 		n : function () {
-
+			if (fife.data.rooms[fife.player.location].exits.length >= 1) {
+				fife.player.location = fife.data.rooms[fife.player.location].exits[0];
+			} else {
+				fife.write("Can't go that way.");
+			}
 		},
 		e : function () {
-
+			if (fife.data.rooms[fife.player.location].exits.length >= 2) {
+				fife.player.location = fife.data.rooms[fife.player.location].exits[1];
+			} else {
+				fife.write("Can't go that way.");
+			}
 		},
 		s : function () {
-
+			if (fife.data.rooms[fife.player.location].exits.length >= 3) {
+				fife.player.location = fife.data.rooms[fife.player.location].exits[2];
+			} else {
+				fife.write("Can't go that way.");
+			}
 		},
 		w : function () {
-
+			if (fife.data.rooms[fife.player.location].exits.length >= 4) {
+				fife.player.location = fife.data.rooms[fife.player.location].exits[3];
+			} else {
+				fife.write("Can't go that way.");
+			}
 		},
 		up : function () {
-
+			if (fife.data.rooms[fife.player.location].exits.length >= 5) {
+				fife.player.location = fife.data.rooms[fife.player.location].exits[4];
+			} else {
+				fife.write("Can't go that way.");
+			}
 		},
 		down : function () {
-
+			if (fife.data.rooms[fife.player.location].exits.length >= 6) {
+				fife.player.location = fife.data.rooms[fife.player.location].exits[5];
+			} else {
+				fife.write("Can't go that way.");
+			}
 		},
 		ne : function () {
-
+			if (fife.data.rooms[fife.player.location].exits.length >= 7) {
+				fife.player.location = fife.data.rooms[fife.player.location].exits[6];
+			} else {
+				fife.write("Can't go that way.");
+			}
 		},
 		se : function () {
-
+			if (fife.data.rooms[fife.player.location].exits.length >= 8) {
+				fife.player.location = fife.data.rooms[fife.player.location].exits[7];
+			} else {
+				fife.write("Can't go that way.");
+			}
 		},
 		sw : function () {
-
+			if (fife.data.rooms[fife.player.location].exits.length >= 9) {
+				fife.player.location = fife.data.rooms[fife.player.location].exits[8];
+			} else {
+				fife.write("Can't go that way.");
+			}
 		},
 		nw : function () {
-
+			if (fife.data.rooms[fife.player.location].exits.length >= 10) {
+				fife.player.location = fife.data.rooms[fife.player.location].exits[9];
+			} else {
+				fife.write("Can't go that way.");
+			}
 		},
 		//
 		look : function () {
-			fife.write(data.rooms[fife.location].look);
+			fife.write(fife.data.rooms[fife.player.location].look);
 		},
 		i : function () {
-
+			var tmp_items = [];
+			for (i in fife.data.items) {
+				if (fife.data.items[i].location === "i") {
+					tmp_items.push(i);
+				}
+			}
+			tmp_items = tmp_items.join(",");
+			fife.write(tmp_items);
 		},
 		score : function () {
-
+			fife.write(fife.player.score);
 		},
 		time : function () {
-
+			fife.write(fife.player.time);
 		},
-		get : function () {
-
+		get : function (item) {
+			if (fife.data.items[item].take == "yes") {
+				fife.data.items[item].location = "i"
+			} else {
+				fife.write("Can't take " + item);
+			}
 		},
 		restart : function () {
 			location.reload();
 		}
-
 	}
-
-
-}
+};
